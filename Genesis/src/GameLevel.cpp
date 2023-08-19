5c990fcba286f5f2c070018e556c86bd5e484343
linuxing3 <linuxing3@qq.com>
2023-08-19 17:24:25 +0800

    [fix] more explicitly self explained variable of pixel coordinate

--- Genesis/src/GameLevel.cpp
+++ Genesis/src/GameLevel.cpp
@@ -22,6 +22,7 @@ Level::~Level() {
 }
 
 void Level::Render(std::shared_ptr<GameRenderer> renderer) {
+  // get pixels of renderer image
   uint32_t* buffer = renderer->GetImageData();
   for (uint32_t j = 0; j < m_Height; j++) {
     if (j > renderer->GetHeight())
@@ -29,11 +30,12 @@ void Level::Render(std::shared_ptr<GameRenderer> renderer) {
     for (uint32_t i = 0; i < m_Width; i++) {
       if (i > renderer->GetWidth())
         break;
+      // get color at pixel coordinate of m_Tiles image
       uint32_t tileID = m_Tiles[i + j * m_Width];
+      // write to renderer image pixel coordinate
       buffer[i + j * renderer->GetWidth()] = tileID;
     }
   }
-  
 };
 
 void Level::LoadLevelFromFile(const std::filesystem::path& path) {
@@ -52,38 +54,53 @@ void Level::RenderBackground(std::shared_ptr<GameRenderer> renderer) {
 
   uint32_t* buffer = renderer->GetImageData();
 
+  // NOTE: Background image size > Renderer viewport size, we calculate how to
+  // fit in
   uint32_t scale[2] = {m_Width / renderer->GetWidth(),
                        m_Height / renderer->GetHeight()};
 
   for (uint32_t j = 0; j < renderer->GetHeight(); j++) {
     for (uint32_t i = 0; i < renderer->GetWidth(); i++) {
-      buffer[i + j * renderer->GetWidth()] =
-         m_Tiles[i / scale[0] + j / scale[1] * m_Width];
+      uint32_t index_within_viewport = i + j * renderer->GetWidth();
+      uint32_t index_within_image = i / scale[0] + m_Width * j / scale[1];
+      buffer[index_within_viewport] = m_Tiles[index_within_image];
     }
   }
 }
 
-void Level::RenderTiles(int cx, int cy, std::shared_ptr<GameRenderer> renderer) {
+void Level::RenderTiles(int cx, int cy,
+                        std::shared_ptr<GameRenderer> renderer) {
   uint32_t* buffer = renderer->GetImageData();
   uint32_t spriteSize = 64;
-  uint32_t xp = cx * spriteSize, yp = cy * spriteSize;
 
-  uint32_t nx = renderer->GetWidth() / spriteSize;
-  uint32_t ny = renderer->GetHeight() / spriteSize;
+  uint32_t x_position_of_sprite_within_sheet = cx * spriteSize;
+  uint32_t y_position_of_sprite_within_sheet = cy * spriteSize;
 
-  uint32_t xt = 0;
-  uint32_t yt = 0;
+  // NOTE: Tile image size < Renderer Viewport size, we calculate how many quads
+  uint32_t tiles_perline = renderer->GetWidth() / spriteSize;
+  uint32_t tiles_percolumn = renderer->GetHeight() / spriteSize;
 
-  for (uint32_t y = 0; y < spriteSize; y++)
-    for (uint32_t x = 0; x < spriteSize; x++) {
+  uint32_t sprite_start_anchor_x = 0;
+  uint32_t sprite_start_anchor_y = 0;
 
-      uint32_t color = m_Grass[(xp + x) + (yp + y) * m_Width];
+  for (uint32_t y_coord_within_sprite = 0; y_coord_within_sprite < spriteSize;
+       y_coord_within_sprite++)
+    for (uint32_t x_coord_within_sprite = 0; x_coord_within_sprite < spriteSize;
+         x_coord_within_sprite++) {
 
-      for (uint32_t j = 0; j < ny; j++) {
-        for (uint32_t i = 0; i < nx; i++) {
-          uint32_t coord_x = xt + x + spriteSize * i;
-          uint32_t coord_y = (yt + y + spriteSize * j) * renderer->GetWidth();
-          buffer[coord_x + coord_y] = color;
+      uint32_t color =
+          m_Grass[(x_position_of_sprite_within_sheet + x_coord_within_sprite) +
+                  (y_position_of_sprite_within_sheet + y_coord_within_sprite) *
+                      m_Width];
+
+      for (uint32_t j = 0; j < tiles_percolumn; j++) {
+        for (uint32_t i = 0; i < tiles_perline; i++) {
+          uint32_t x_coord =
+              sprite_start_anchor_x + x_coord_within_sprite + spriteSize * i;
+          uint32_t y_coord =
+              (sprite_start_anchor_y + y_coord_within_sprite + spriteSize * j) *
+              renderer->GetWidth();
+          buffer[x_coord + y_coord] = color;
         }
       }
     };
--- Genesis/src/GameRenderer.h
+++ Genesis/src/GameRenderer.h
@@ -18,10 +18,13 @@ class GameRenderer {
   void ClearSingleColor(uint32_t color = 0xff7f007f);
 
   uint32_t *GetImageData() { return m_ImageData; };
-  std::shared_ptr<Walnut::Image> GetFinalImage() const { return m_FinalImage; }
+  [[nodiscard]] std::shared_ptr<Walnut::Image> GetFinalImage() const {
+    return m_FinalImage;
+  }
 
   uint32_t GetWidth() { return m_FinalImage->GetWidth(); };
   uint32_t GetHeight() { return m_FinalImage->GetHeight(); };
+
  private:
   std::shared_ptr<Walnut::Image> m_FinalImage;
   uint32_t *m_ImageData = nullptr;
